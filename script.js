ec07922cb6d0690b03814b5d83c01d49ded79a6c
Lincoln-Dupree <lincoln.vfx@gmail.com>
2025-10-14 02:44:20 -0500

    started working on the game.html

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-"use strict"; 
+"use strict";
+
+const quizSelectBtn = document.querySelectorAll(".topic-btn");
+
+async function getData() {
+    try {
+        const response = await fetch("./data/data.json")
+
+        if (!response.ok) {
+            throw new Error("Could not fetch resource")
+        }
+
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
