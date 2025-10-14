c2f4c7847736d9d0785672cedc0630dc99e17492
Lincoln-Dupree <lincoln.vfx@gmail.com>
2025-10-14 05:59:03 -0500

    got the first page of the quiz to update based on JSON data -- only first question at the moment.

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const quizSelectBtn = document.querySelectorAll(".topic-btn");
 for (const btn of quizSelectBtn) {
 
     btn.addEventListener("click", async function () {
+
         const topicText = btn.querySelector(".topic-text").innerText;
         const data = await getData();
         const quizzes = data.quizzes;
@@ -32,9 +33,32 @@ for (const btn of quizSelectBtn) {
             }
         }
 
-
-
+        localStorage.setItem("quizData", JSON.stringify(quiz));
         window.location.href = "game.html";
-        console.log(quiz);
     })
 }
+
+const quizTitleIcon = document.querySelector(".quiz-title-icon");
+const quizTitleText = document.querySelector(".quiz-title-text");
+const questionText = document.querySelector(".question-text");
+const choiceAText = document.querySelector(".choice-a-text");
+const choiceBText = document.querySelector(".choice-b-text");
+const choiceCText = document.querySelector(".choice-c-text");
+const choiceDText = document.querySelector(".choice-d-text");
+
+window.addEventListener("DOMContentLoaded", function () {
+    if (document.body.classList.contains("page-game")) {
+        const quizData = JSON.parse(localStorage.getItem("quizData"));
+
+        const selectData = quizData[0];
+        console.log(selectData.questions[0].options);
+
+        quizTitleIcon.src = selectData.icon;
+        quizTitleText.innerText = selectData.title;
+        questionText.innerText = selectData.questions[0].question;
+        choiceAText.innerText = selectData.questions[0].options[0];
+        choiceBText.innerText = selectData.questions[0].options[1];
+        choiceCText.innerText = selectData.questions[0].options[2];
+        choiceDText.innerText = selectData.questions[0].options[3];
+    }
+});
